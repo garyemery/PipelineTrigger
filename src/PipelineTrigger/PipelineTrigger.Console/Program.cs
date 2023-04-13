99545e31c536dbeaad8b1d3397a8441d857bb038
garyemery <garyemery@me.com>
2023-04-13 22:41:45 +0100

    Console change

--- src/PipelineTrigger/PipelineTrigger.Console/Program.cs
+++ src/PipelineTrigger/PipelineTrigger.Console/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! A new change to console");
+Console.WriteLine("Hello, World! Another change to console");
