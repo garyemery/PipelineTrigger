a994a41ca23557fc7c2fa94220cbb262f2f20728
garyemery <garyemery@me.com>
2023-04-13 22:54:47 +0100

    git diff changes

--- src/PipelineTrigger/PipelineTrigger.Console/Program.cs
+++ src/PipelineTrigger/PipelineTrigger.Console/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Another remove change to console");
+Console.WriteLine("Hello, World! Another change to console");
