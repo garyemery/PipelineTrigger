8d0324de8fa64830b80730b1526baaca50e6a135
garyemery <garyemery@me.com>
2023-04-14 11:28:41 +0100

    Still build shared

--- src/PipelineTrigger/PipelineTrigger.Console/Program.cs
+++ src/PipelineTrigger/PipelineTrigger.Console/Program.cs
@@ -3,4 +3,4 @@ Console.WriteLine("Hello, World! Another change to CNSL to count");
 // PLS, PLS! HAWAY - and some console changes
 // Changes here and Shared
 
-// Branch changes, in console. 
+// Branch changes, in console. Plus a bit more, should still build shared.
