23891d1acda9436208eeb3180d7c9e51f284f433
garyemery <garyemery@me.com>
2023-04-14 11:18:00 +0100

    Set pipeline properly, now with some actual console changes

--- src/PipelineTrigger/PipelineTrigger.Console/Program.cs
+++ src/PipelineTrigger/PipelineTrigger.Console/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! Another change to CNSL to count");
-// PLS, PLS! HAWAY - Set vars with some other way
+// PLS, PLS! HAWAY - and some console changes
